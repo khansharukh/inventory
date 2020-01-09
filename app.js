b68dd070125f22bde2ba9d3b32a65cb56177920d
Khan Sharukh <=>
2020-01-09 14:55:29 +0530

    Create database and tables as required

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
 // create express app
 const app = express();
+
+// create database inventory and add db credentials and details
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: 'inventory',
+    multipleStatements: true
+});
+// connection to db
+con.connect(function(err) {
+    if (err) throw err;
+    // console.log("Connected!");
+
+    var tbl_products = "CREATE TABLE IF NOT EXISTS `inventory`.`products` ( `id` INT(11) NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) NOT NULL , `description` TEXT NOT NULL , `in_stock` ENUM('yes','no') NOT NULL DEFAULT 'yes' , `created_at` DATETIME NOT NULL , PRIMARY KEY (`id`))";
+    con.query(tbl_products, function (err, result) {
+        if (err) throw err;
+        //console.log("Inventory table created");
+    });
+    var tbl_users = "CREATE TABLE IF NOT EXISTS `inventory`.`users` ( `id` INT(11) NOT NULL AUTO_INCREMENT , `name` VARCHAR(50) NOT NULL , `email` VARCHAR(100) NOT NULL , `password` VARCHAR(255) NOT NULL , `created_at` DATETIME NOT NULL , PRIMARY KEY (`id`))";
+    con.query(tbl_products, function (err, result) {
+        if (err) throw err;
+        //console.log("User table created");
+    });
+});
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
